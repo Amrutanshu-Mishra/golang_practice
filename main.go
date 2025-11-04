bb99a7af8f8e3a470f0e1c4e715886bb2921948c
Amrutanshu <amrutanshumishra607@gmail.com>
2025-11-04 13:15:35 +0530

    feat: learn about the different data types

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, ninjas!")
+
+	//strings
+	// var nameOne string = "som";//cant use single quotes
+	// var nameTwo = "mishra";
+	// var nameThree string;
+	// fmt.Println(nameOne, nameTwo, nameThree);
+
+	// nameOne = "Amrutanshu"
+	// nameThree = "bowser"
+	// fmt.Println(nameOne, nameTwo, nameThree);
+
+	// namefour:="yoshi"
+	// fmt.Println(namefour);
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits and memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint8 = 25
+
+	// var scoreOne float32 = -25.98
+	// var scoreTwo float64 = 100.89232567
+	// scoreThree := 1.5
 }
