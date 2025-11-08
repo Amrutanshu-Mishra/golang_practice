88deec87f133c446b44dab247e9f6f074bd080fe
Amrutanshu <amrutanshumishra607@gmail.com>
2025-11-08 23:44:24 +0530

    practice about boolean

--- main.go
+++ main.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	// greeting := "hello there friends"
-	// fmt.Println(strings.Contains(greeting, "hello"))
-	// fmt.Println(strings.ReplaceAll(greeting, "hello", "Hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Split(greeting, " "))
-	// fmt.Println(strings.Index(greeting, "ll"))
-	// fmt.Println(greeting)
+	age := 45
 
-	ages:= []int{45, 20, 35, 30, 75, 60, 50, 25}
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
 
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index:=sort.SearchInts(ages, 30)
-	fmt.Println(index)
-
-	names:= []string{"yoshi", "mario", "peach", "bowser", "luigi"}
-	sort.Strings(names)
-	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "bowser"))
 }
